Trying to fix underflow error - Not solved

cnn_main.py
@@ -1,5 +1,5 @@
 # %%
-from scratch.layers import Conv, MaxPool, Dense, Flatten
+from scratch.layers import conv, maxpool, dense, flatten
 from scratch.activations import ReLU, Softmax
 from scratch.loss import CategoricalCrossEntropy
 import numpy as np
@@ -31,12 +31,11 @@ for x in tqdm(range(dataset_size)):
     img = (img - np.min(img)) / np.ptp(img)
     data[x] = img
 
-conv_l = Conv(num_filters=4, kernel_size=(3, 3), padding=1, stride=1, log=False)
-pool_l = MaxPool()
-pool_l2 = MaxPool()
-flatten_l = Flatten()
-dense_l = Dense(num_neurons=8, activation=ReLU())
-dense_softmax = Dense(num_neurons=2, activation=Softmax())
+pool_l = maxpool()
+pool_l2 = maxpool()
+flatten_l = flatten()
+dense_l = dense(num_neurons=8, activation=ReLU())
+dense_softmax = dense(num_neurons=2, activation=Softmax())
 
 loss_function = CategoricalCrossEntropy()
 

scratch/layers/conv.py
@@ -1,5 +1,5 @@
 from scratch.activations import Activation, ReLU
-from scratch.layers.Layer import Layer, LayerType
+from scratch.layers.layer import Layer, LayerType
 import numpy as np
 
 
@@ -17,6 +17,7 @@ class Conv(Layer):
 
         # TODO add bias?
         self.filters = None
+        self.d_filters = None
         self.last_input = None
 
     def setup(self, input_shape: np.ndarray):
@@ -84,8 +85,8 @@ class Conv(Layer):
                 for f in range(self.num_filters):
                     d_filters[:, :, :, f] += d_score[b, i, j, f] * img_region[b]
 
-        self.update(d_score=d_filters)
+        self.d_filters = d_filters
         return d_score
 
-    def update(self, d_score: np.ndarray, learn_rate: float = 1e-0) -> None:
-        self.filters = self.filters - (d_score * learn_rate)
+    def update(self, learn_rate: float = 1e-0) -> None:
+        self.filters = self.filters + (-learn_rate * self.d_filters)

scratch/layers/dense.py
@@ -1,5 +1,5 @@
 from scratch.activations import Activation, ReLU
-from scratch.layers.Layer import Layer, LayerType
+from scratch.layers.layer import Layer, LayerType
 import numpy as np
 
 
@@ -18,11 +18,11 @@ class Dense(Layer):
         self.next_layer = None
         self.input_layer = None
 
-    def setup(self, input_shape, next_layer=None) -> None:
+    def setup(self, input_shape: tuple, next_layer=None) -> None:
         self.input_shape = input_shape
-        self.batch_size: int = input_shape[1]
+        self.batch_size: int = input_shape[0]
         # multiply by 0.1 to reduce the variance of our initial values
-        self.W: np.ndarray = 0.1 * np.random.randn(self.batch_size, self.num_neurons)
+        self.W: np.ndarray = 0.1 * np.random.randn(input_shape[1], self.num_neurons)
         self.b: np.ndarray = np.zeros((1, self.num_neurons))
         self.next_layer: np.ndarray = next_layer
         self.output_shape: tuple[int, int] = (self.batch_size, self.num_neurons)
@@ -42,7 +42,6 @@ class Dense(Layer):
             d_score = self.activation.backpropagation(d_score, self.next_layer.W)
         self.dW: np.ndarray = np.dot(self.input_layer.T, d_score)
         self.db: np.ndarray = np.sum(d_score, axis=0, keepdims=True)
-        self.update()
         return d_score
 
     def update(self, step_size: float = 1e-0) -> None:

scratch/loss.py
@@ -34,7 +34,7 @@ class CategoricalCrossEntropy(Loss):
         y_pred_clipped = np.clip(self.y_pred, 1e-7, 1 - 1e-7)
         correct_confidences = y_pred_clipped[range(samples), y_true]
         negative_log_likelihood = -np.log(correct_confidences)
-        acc = sum([1 for i in range(samples) if np.argmax(y_pred[i]) == y_true[i]]) / samples
+        acc = sum([1 for i in range(samples) if np.argmax(self.y_pred[i]) == self.y_true[i]]) / samples
         return negative_log_likelihood, acc
 
     def backpropagation(self):

scratch/model.py
@@ -1,6 +1,6 @@
 from scratch.loss import CategoricalCrossEntropy, Loss
 import numpy as np
-from scratch.layers.Layer import Layer, LayerType
+from scratch.layers.layer import Layer, LayerType
 
 
 class Model:
@@ -54,10 +54,15 @@ class Model:
                     print_loss = "{:.2}".format(loss)
                     print_acc = "{:.2%}".format(acc)
                     print(f"iteration {i}: loss {print_loss} |  acc {print_acc}")
+                    print(np.sum(d_score))
 
                 # backward step
                 for layer in reversed(self.layers):
                     d_score = layer.backpropagation(d_score=d_score)
+
+                for layer in self.layers:
+                    if layer.layer_type == LayerType.CONV or layer.layer_type == LayerType.DENSE:
+                        layer.update()
 
     def layer_setup(self, layer: Layer, layer_idx: int, input_shape: tuple):
         if layer.layer_type == LayerType.DENSE: