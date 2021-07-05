Conv layer backpropagation bug fixed

scratch/activations.py
@@ -35,7 +35,7 @@ class LeakyReLU(Activation):
         return self.output
 
     def backpropagation(self, d_score: np.ndarray) -> np.ndarray:
-        d_score[self.output <= 0] = self.param
+        d_score[self.output <= 0] = self.param * d_score[self.output]
         return d_score
 
 

scratch/layers/conv.py
@@ -24,12 +24,16 @@ class Conv(Layer):
         self.last_input = None
 
     def setup(self, input_shape: tuple):
+        if len(input_shape) == 4:
+            input_shape = input_shape[1:]
+
         self.input_shape: tuple = input_shape
         self.output_shape: tuple = self.convolution_compatibility(input_shape)
 
-        # We divide by 10 to reduce the variance of our initial values
+        # / np.sqrt(self.input_shape) <- Xavier initialization
         self.filters: np.ndarray = np.random.random_sample(
-            (self.kernel_size[0], self.kernel_size[1], self.input_shape[2], self.num_filters)) * 0.01
+            (self.kernel_size[0], self.kernel_size[1], self.input_shape[2], self.num_filters)) / np.sqrt(
+            self.kernel_size[0] * self.kernel_size[1] * self.input_shape[2])
 
         # print(
         #    f"Conv layer\ninput: {self.input_shape}\nfilter: {self.filters.shape}\noutput: {self.output_shape}\n")
@@ -65,8 +69,6 @@ class Conv(Layer):
                 yield img_region, i, j
 
     def forward(self, inputs: np.ndarray) -> np.ndarray:
-        print("F CONV")
-        print(inputs.shape)
         self.batch_size: int = inputs.shape[0]
         if self.padding > 0:
             inputs = self.zero_padding(inputs, self.padding)
@@ -74,26 +76,22 @@ class Conv(Layer):
         output = np.zeros((self.batch_size,) + self.output_shape)
         volume_depth = output.shape[3]
 
-        for img_region, i, j in self.iterate_regions(inputs):
-            for d in range(volume_depth):
+        for d in range(volume_depth):
+            for img_region, i, j in self.iterate_regions(inputs):
                 output[:, i, j, d] = np.sum(img_region * self.filters[:, :, :, d], axis=(1, 2, 3))
-        print(output.shape)
         return output
 
     def backpropagation(self, d_score: np.ndarray) -> np.ndarray:
-        print("BP CONV")
-        # TODO fix output shape
-        print(d_score.shape)
-        d_filters = np.zeros(self.filters.shape)
-
-        for img_region, i, j in self.iterate_regions(self.last_input):
-            for b in range(d_score.shape[0]):
-                for f in range(d_score.shape[3]):
-                    d_filters[:, :, :, f] += d_score[b, i, j, f] * img_region[b]
-
-        self.d_filters = d_filters
-        print(d_score.shape)
-        return d_score
+        self.d_filters = np.zeros(self.filters.shape)
+        new_d_score = np.zeros(self.last_input.shape)
+        # filters update
+        for b in range(self.batch_size):
+            for img_region, i, j in self.iterate_regions(self.last_input):
+                for f in range(self.num_filters):
+                    self.d_filters[:, :, :, f] += np.dot(d_score[b, i, j, f], img_region[b])
+                    new_d_score[b, i:i + self.kernel_size[0], j:j + self.kernel_size[1], :] += \
+                        self.filters[:, :, :, f] * d_score[b, i, j, f]
+        return new_d_score
 
     def update(self, learn_rate: float = 1e-0) -> None:
         self.filters = self.filters + (-learn_rate * self.d_filters)

scratch/layers/dense.py
@@ -20,8 +20,8 @@ class Dense(Layer):
 
     def setup(self, input_shape: int, next_layer: Layer = None) -> None:
         self.input_shape = input_shape
-        # multiply by 0.1 to reduce the variance of our initial values
-        self.W: np.ndarray = 0.1 * np.random.randn(self.input_shape, self.num_neurons)
+        # / np.sqrt(self.input_shape) <- Xavier initialization
+        self.W: np.ndarray = np.random.randn(self.input_shape, self.num_neurons) / np.sqrt(self.input_shape)
         self.b: np.ndarray = np.zeros((1, self.num_neurons))
         self.next_layer: Layer = next_layer
         self.output_shape: int = self.num_neurons
@@ -40,7 +40,6 @@ class Dense(Layer):
         if self.next_layer is not None:
             d_score = self.activation.backpropagation(d_score)
         self.dW: np.ndarray = np.dot(self.input_layer.T, d_score)
-        # TODO check db computation
         self.db: np.ndarray = np.sum(d_score, axis=0, keepdims=True)
         d_score = np.dot(d_score, self.W.T)
         return d_score

scratch/layers/maxpool2.py
@@ -4,7 +4,7 @@ from scratch.layers.layer import Layer, LayerType
 import numpy as np
 
 
-class MaxPool(Layer):
+class MaxPool2(Layer):
 
     def __init__(self):
         super().__init__()
@@ -26,32 +26,22 @@ class MaxPool(Layer):
                 yield img_region, i, j
 
     def forward(self, inputs: np.ndarray) -> np.ndarray:
-        print("F MAXPOOL")
-        print(inputs.shape)
         self.last_input: np.ndarray = inputs
         batch_size, h, w, d = inputs.shape
         output = np.zeros((batch_size, h // 2, w // 2, d))
 
         for img_region, i, j in self.iterate_regions(inputs):
             output[:, i, j, :] = np.amax(img_region, axis=(1, 2))
-        print(output.shape)
         return output
 
     def backpropagation(self, d_score: np.ndarray) -> np.ndarray:
-        print("BP MAXPOOL")
-        print(d_score.shape)
         d_input = np.zeros(self.last_input.shape)
-
         for img_region, i, j in self.iterate_regions(self.last_input):
             _, h, w, _ = img_region.shape
             region_max = np.amax(img_region, axis=(1, 2))
-
             for idx_h in range(h):
                 for idx_w in range(w):
                     # If the pixel was the max value, copy the gradient to it.
                     if (img_region[:, idx_h, idx_w, :] == region_max).all:
-
                         d_input[:, i * 2 + idx_h, j * 2 + idx_w, :] = d_score[:, i, j, :]
-
-        print(d_input.shape)
         return d_input

scratch/loss.py
@@ -41,5 +41,5 @@ class CategoricalCrossEntropy(Loss):
         num_examples = len(self.y_pred)
         d_score = self.y_pred
         d_score[range(num_examples), self.y_true] -= 1
-        d_score /= num_examples
+        d_score = np.divide(d_score, num_examples)
         return d_score