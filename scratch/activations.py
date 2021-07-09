Fixed underflow problem in exp

scratch/activations.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 np.seterr(all='raise')
 
 
@@ -42,9 +41,13 @@ class LeakyReLU(Activation):
 class Softmax(Activation):
     def compute(self, inputs: np.ndarray) -> np.ndarray:
         # subtract the max (prevent overflow)
-        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
-        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
-        return probabilities
+        # exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        # probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        # return probabilities
+        log = np.log(np.sum(np.exp(inputs), axis=1))
+        sub = np.subtract(inputs.T, log).T
+        exp = np.exp(sub)
+        return exp
 
     def backpropagation(self, d_score=None, layer=None) -> None:
         # in this implementation the Categorical Cross Entropy Loss function

scratch/layers/conv.py
@@ -4,7 +4,7 @@ from scratch.activations import Activation, ReLU
 from scratch.layers.layer import Layer, LayerType
 import numpy as np
 from tqdm import tqdm
-
+import time
 
 class Conv(Layer):
 
@@ -127,7 +127,7 @@ class Conv(Layer):
         new_d_score = np.zeros(self.last_input.shape)
         batch_size = d_score.shape[0]
 
-        # filters update
+        # filters delta
         if len(self.input_shape) == 2:
             for b in range(batch_size):
                 for img_region, i, j in self.iterate_regions(self.last_input, kernel=self.kernel_size,stride=self.stride):
@@ -138,6 +138,7 @@ class Conv(Layer):
         elif len(self.input_shape) == 3:
             for b in range(batch_size):
                 for img_region, i, j in self.iterate_regions(self.last_input, kernel=self.kernel_size,stride=self.stride):
+
                     for f in range(self.num_filters):
                         self.d_filters[:, :, :, f] += np.dot(d_score[b, i, j, f], img_region[b])
                         new_d_score[b, i:i + self.kernel_size[0], j:j + self.kernel_size[1], :] += self.filters[:, :, :, f] * d_score[b, i, j, f]

test.py
@@ -77,5 +77,45 @@ data = np.array(data)
 print(np.amax(data, axis=(1, 2)))
 
 # %%
-z = [2,3,-2,4,1,-2]
+z = [2, 3, -2, 4, 1, -2]
 print(np.greater(z, 0).astype(int))
+
+# %%
+
+a = [[1, 2, 3, 4, 5],
+     [6, 7, 8, 9, 10],
+     [11, 12, 13, 14, 15],
+     [16, 17, 18, 19, 20],
+     [21, 22, 23, 24, 25]]
+
+a = np.array(a)
+
+print(a[1:1 + 3, 1:1 + 3])
+
+
+# %%
+
+def compute2(inputs: np.ndarray) -> np.ndarray:
+    log = np.log(np.sum(np.exp(inputs), axis=1))
+    sub = np.subtract(inputs.T, log).T
+    exp = np.exp(sub)
+    return exp
+
+
+def compute1(inputs: np.ndarray) -> np.ndarray:
+    # subtract the max (prevent overflow)
+    exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+    probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+    # probabilities = np.exp(inputs - np.log(np.sum(inputs, axis=1)))
+    return probabilities
+
+
+inp = [[0.6, 0.4, 0.6],
+       [0.4, 0.1, 0.6],
+       [0.3, 0.2, 0.2],
+       [0.9, 0.7, 0.8]]
+
+inp = np.array(inp)
+
+print(compute1(inp))
+print(compute2(inp))