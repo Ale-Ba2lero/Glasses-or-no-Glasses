Leaky relu fix and Conv backprop rework

cnn_main.py
@@ -5,6 +5,7 @@ from model.layers.dense import Dense
 from model.layers.maxpool2 import MaxPool2
 from model.layers.flatten import Flatten
 from model.layers.ReLU import ReLU
+from model.layers.leakyReLU import LeakyReLU
 from model.layers.softmax import Softmax
 from model.loss import CategoricalCrossEntropy
 import numpy as np
@@ -53,98 +54,22 @@ N_EPOCHS = 3
 BATCH_SIZE = len(X_train) // len(X_train)
 
 # ------------------------------------ BUILD THE MODEL
-"""nn = Model([
-    Conv(num_filters=10, padding=1), ReLU(),
+nn = Model([
+    Conv(num_filters=10, padding=1), LeakyReLU(),
+    Conv(num_filters=2, padding=1), LeakyReLU(),
     MaxPool2(),
     Flatten(),
     Dense(10), Softmax()
-], CategoricalCrossEntropy())"""
-
-forward_time = 0
-backward_time = 0
-
-conv = Conv(num_filters=10, padding=1)
-relu = ReLU()
-conv2 = Conv(num_filters=10, padding=1)
-relu2 = ReLU()
-mp = MaxPool2()
-flatten = Flatten()
-dense = Dense(10)
-softmax = Softmax()
-
-loss_function = CategoricalCrossEntropy()
-
-conv.setup(X_train[0].shape)
-relu.setup(conv.output_shape)
-conv2.setup(relu.output_shape)
-relu2.setup(conv2.output_shape)
-mp.setup(relu2.output_shape)
-flatten.setup(mp.output_shape)
-dense.setup(flatten.output_shape)
+], CategoricalCrossEntropy())
 
 # ------------------------------------ TRAIN THE MODEL
-"""nn.train(X=X_train,
+nn.train(X=X_train,
          y=y_train,
          epochs=N_EPOCHS,
          batch_size=BATCH_SIZE,
          step_size=STEP_SIZE,
-         log_freq=1)"""
-n_batches: int = len(X_train) // BATCH_SIZE
-extra_batch: int = int(len(X_train) % BATCH_SIZE > 0)
-
-for i in tqdm(range(N_EPOCHS)):
-    loss = 0
-    num_correct = 0
-    for j in range(n_batches + extra_batch):
-        if j > 0 and j % 100 == 99:
-            print_loss = "{:.2}".format(loss / 100)
-            print_acc = "{:.2%}".format(num_correct / 100)
-            print(f"\niteration {j + 1}: loss {print_loss} |  acc {print_acc}")
-            conv.print_time()
-            loss = 0
-            num_correct = 0
-
-        X_batch = X_train[j * BATCH_SIZE:(j + 1) * BATCH_SIZE]
-        y_batch = y_train[j * BATCH_SIZE:(j + 1) * BATCH_SIZE]
-
-        out = conv.forward(X_batch)
-        out = relu.forward(out)
-        out = conv2.forward(out)
-        out = relu2.forward(out)
-        out = mp.forward(out)
-        out = flatten.forward(out)
-        out = dense.forward(out)
-        out = softmax.forward(out)
-
-        l, acc, d_score = loss_function.calculate(out, y_batch)
-        loss += l
-        num_correct += acc
-
-        d_score = dense.backpropagation(d_score)
-        d_score = flatten.backpropagation(d_score)
-        d_score = mp.backpropagation(d_score)
-        d_score = relu2.backpropagation(d_score)
-        d_score = conv2.backpropagation(d_score)
-        d_score = relu.backpropagation(d_score)
-        d_score = conv.backpropagation(d_score)
-
-        dense.update(STEP_SIZE)
-        conv.update(STEP_SIZE)
-        conv2.update(STEP_SIZE)
+         log_freq=1)
 
 # ------------------------------------ EVALUTATE THE MODEL
-"""nn.evaluate(X_test=X_test, y_test=y_test)"""
-
-output = conv.forward(X_test)
-output = relu.forward(output)
-output = conv2.forward(X_test)
-output = relu2.forward(output)
-output = mp.forward(output)
-output = flatten.forward(output)
-output = dense.forward(output)
-output = softmax.forward(output)
-
-predicted_class = np.argmax(output, axis=1)
-accuracy = "{:.2%}".format(np.mean(predicted_class == y_test))
-print(f'Test accuracy: {accuracy}')
+nn.evaluate(X_test=X_test, y_test=y_test)
 

model/layers/conv.py
@@ -1,5 +1,4 @@
 import sys
-from numba import jit
 from model.activations import Activation, ReLU
 from model.layers.layer import Layer, LayerType
 import numpy as np
@@ -7,28 +6,28 @@ from tqdm import tqdm
 import time
 
 
-@jit(nopython=True)
 def iterate_regions(inputs: np.ndarray, kernel: int = 3, stride: int = 1) -> (
         np.ndarray, int, int):
-    b: int = 0
     h: int = 0
     w: int = 0
+    img_region = None
 
     if len(inputs.shape) == 3:
         b, h, w = inputs.shape
-    """elif len(inputs.shape) == 4:
-        _, h, w, _ = inputs.shape"""
+        img_region = np.zeros((b, kernel, kernel))
+    elif len(inputs.shape) == 4:
+        b, h, w, d = inputs.shape
+        img_region = np.zeros((b, kernel, kernel, d))
 
     h_limit = h - kernel + 1
     w_limit = w - kernel + 1
-    img_region = np.zeros((b, kernel, kernel))
 
     for i in range(0, h_limit, stride):
         for j in range(0, w_limit, stride):
             if len(inputs.shape) == 3:
                 img_region = inputs[:, i:(i + kernel), j:(j + kernel)]
-            """elif len(inputs.shape) == 4:
-                img_region = inputs[:, i:(i + kernel), j:(j + kernel), :]"""
+            elif len(inputs.shape) == 4:
+                img_region = inputs[:, i:(i + kernel), j:(j + kernel), :]
             yield img_region, i, j
 
 
@@ -67,7 +66,6 @@ class Conv(Layer):
         self.b_time = 0
 
     def setup(self, input_shape: tuple):
-
         """if len(input_shape) == 4:
             input_shape = input_shape[1:]"""
 
@@ -79,10 +77,10 @@ class Conv(Layer):
             self.filters: np.ndarray = np.random.random_sample(
                 (self.kernel_size, self.kernel_size, self.num_filters)) / np.sqrt(self.kernel_size ** 2)
 
-        """elif len(input_shape) == 3:
+        elif len(input_shape) == 3:
             self.filters: np.ndarray = np.random.random_sample(
                 (self.kernel_size, self.kernel_size, self.input_shape[2], self.num_filters)) / np.sqrt(
-                self.kernel_size * self.kernel_size * self.input_shape[2])"""
+                self.kernel_size * self.kernel_size * self.input_shape[2])
 
         """self.b: np.ndarray = np.zeros((1, self.num_filters))"""
 
@@ -90,8 +88,8 @@ class Conv(Layer):
         h, w = 0, 0
         if len(input_shape) == 2:
             h, w = input_shape
-        """elif len(input_shape) == 3:
-            h, w, _ = input_shape"""
+        elif len(input_shape) == 3:
+            h, w, _ = input_shape
 
         s = self.stride
         p = self.padding
@@ -105,11 +103,10 @@ class Conv(Layer):
         return int(output_layer_h), int(output_layer_w), self.num_filters
 
     def forward(self, inputs: np.ndarray) -> np.ndarray:
-
+        self.last_input: np.ndarray = inputs
         if self.padding > 0:
             inputs = zero_padding(inputs, self.padding)
 
-        self.last_input: np.ndarray = inputs
         batch_size: int = inputs.shape[0]
         output = np.zeros((batch_size,) + self.output_shape)
 
@@ -134,26 +131,27 @@ class Conv(Layer):
         # filters delta
         start = time.time()
         batch_size = d_score.shape[0]
-        if len(d_score.shape) == 4:
-            for b in range(batch_size):
-                for img_region, i, j in iterate_regions(self.last_input, kernel=self.kernel_size, stride=self.stride):
-                    for f in range(self.num_filters):
-                        self.d_filters[:, :, f] += np.dot(d_score[b, i, j, f], img_region[b])
 
-                    # TODO exeute this only if there is another layer before
-                    f_filters = self.filters.reshape(self.kernel_size ** 2, self.num_filters)
-                    f_d_score = d_score[b, i, j].flatten()
-                    prod_ = (f_filters * f_d_score.T).reshape(self.kernel_size, self.kernel_size, self.num_filters)
-                    sum_ = np.sum(prod_, axis=2)
-                    new_d_score[b, i:i + self.kernel_size, j:j + self.kernel_size] += sum_
+        for b in range(batch_size):
+            for img_region, i, j in iterate_regions(self.last_input, kernel=self.kernel_size, stride=self.stride):
+                for f in range(self.num_filters):
+                    if len(self.d_filters.shape) == 3:
+                        self.d_filters[:, :, f] += d_score[b, i, j, f] * img_region[b]
+                    elif len(self.d_filters.shape) == 4:
+                        self.d_filters[:, :, :, f] += d_score[b, i, j, f] * img_region[b]
+
+                # TODO execute this only if there is another layer before
+                flat_filters_size = np.prod(self.filters.shape[:-1])
+                filter_shape = self.filters.shape[:-1]
+                f_filters = self.filters.reshape(flat_filters_size, self.num_filters)
+                f_d_score = d_score[b, i, j].flatten()
+                prod_ = (f_filters * f_d_score.T).reshape(flat_filters_size, self.num_filters)
+                sum_ = np.sum(prod_, axis=1)
+                new_d_score[b, i:i + self.kernel_size, j:j + self.kernel_size] = sum_.reshape(filter_shape)
+
+                # new_d_score[b, i:i + self.kernel_size, j:j + self.kernel_size, :] += self.filters[:, :, :, f] * d_score[ b, i, j, f]
         end = time.time()
         self.b_time += (end - start)
-        """elif len(self.input_shape) == 3:
-            for b in range(batch_size):
-                for img_region, i, j in self.iterate_regions(self.last_input, kernel=self.kernel_size, stride=self.stride):
-                    for f in range(self.num_filters):
-                        self.d_filters[:, :, :, f] += np.dot(d_score[b, i, j, f], img_region[b])
-                        new_d_score[b, i:i + self.kernel_size, j:j + self.kernel_size, :] += self.filters[:, :, :, f] * d_score[ b, i, j, f]"""
         return new_d_score
 
     def update(self, learn_rate: float = 1e-0) -> None:

model/layers/leakyReLU.py
@@ -21,6 +21,5 @@ class LeakyReLU(Layer):
         return self.output
 
     def backpropagation(self, d_score: np.ndarray) -> np.ndarray:
-        backprop = lambda x: self.alpha if x < 0 else 1
-        return np.vectorize(backprop)(d_score)
-
+        d_score[self.output <= 0] *= self.alpha
+        return d_score